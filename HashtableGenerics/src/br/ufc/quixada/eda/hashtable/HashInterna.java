d4690c3e2f7b588dc8afdb6b5d367b398b4b8196
Diego Ávila <nosliper@users.noreply.github.com>
2017-05-09 01:07:28 -0300

    Changed from primitive array to ArrayList
    Due to some limitations of java , I had to change and adapt it to ArrayList instead.

--- HashtableGenerics/src/br/ufc/quixada/eda/hashtable/HashInterna.java
+++ HashtableGenerics/src/br/ufc/quixada/eda/hashtable/HashInterna.java
@@ -1,14 +1,16 @@
 package br.ufc.quixada.eda.hashtable;
 
+import java.util.ArrayList;
+
 public class HashInterna<T> extends Hash<T>{
-	private Node<T>[] table = null;
+	private ArrayList<Node<T>> table = null;
 	private int size = 0;
 	private int emptyKey = -1;
 	public HashInterna(int maxSize) {
 		this.maxSize = maxSize;
-		this.table = new Node<T>[this.maxSize];
-		for (int i = 0; i < table.length; i++) {
-			table[i] = empty();
+		this.table = new ArrayList<Node<T>>();
+		for (int i = 0; i < this.maxSize; i++) {
+			table.add(empty());
 		}
 	}
 	private Node<T> empty() {
@@ -16,16 +18,16 @@ public class HashInterna<T> extends Hash<T>{
 	}
 	protected int hash(int key) {
 		int index = key % maxSize;
-		while(table[index].getKey() != emptyKey) { //TODO: fix error with keys bigger than the maxSize
+		while(table.get(index).getKey() != emptyKey) { //TODO: fix error with keys bigger than the maxSize
 			index = (index + 1) % maxSize;
 		}
 		return index;
 	}
 	public boolean existsKey(int key) {
-		return table[hash(key)].getKey() != emptyKey;
+		return table.get(hash(key)).getKey() != emptyKey;
 	}
 	public T get(int key) {
-		return table[hash(key)].getValue();
+		return table.get(hash(key)).getValue();
 	}
 	public boolean insert(T value, int key) throws FullTableException {
 		try{
@@ -33,8 +35,8 @@ public class HashInterna<T> extends Hash<T>{
 			if (getSize() == maxSize) {
 				throw new FullTableException();
 			}
-			else if (table[index].getKey() == emptyKey) {
-				table[index] = new Node<T>(value, key);
+			else if (table.get(index).getKey() == emptyKey) {
+				table.set(index, new Node<T>(value, key));
 				size += 1;
 				return true;
 			}
@@ -45,11 +47,10 @@ public class HashInterna<T> extends Hash<T>{
 		}
 		return false;
 	}
-	@SuppressWarnings("unchecked")
 	public boolean remove(int key) {
 		int index = hash(key);
-		if (table[index].getKey() != emptyKey) { 
-			table[index] = empty();
+		if (table.get(index).getKey() != emptyKey) { 
+			table.set(index, empty());
 			size -= 1;
 			return true;
 		}
